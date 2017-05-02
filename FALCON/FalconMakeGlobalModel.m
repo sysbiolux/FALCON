3b29d44aaab3b71b48520b3f09ec247c542398ea
philippelucarelli <philippe.lucarelli@uni.lu>
2017-05-02 12:49:30 +0200

    update

--- FALCON/FalconMakeGlobalModel.m
+++ FALCON/FalconMakeGlobalModel.m
@@ -188,7 +188,7 @@ for m=1:length(MeasFileList)
         Input_index=[];
         Output_index=[];
 
-        Input_vector=cell2mat(OtherIn(2:end,:));
+        Input_vector=cell2mat(OtherIn(2:end,2:end));
         Annotation = (OtherIn(2:end,1));    
         for jj=2:length(OtherIn(:,2))   %%%% modif Philippe OtherIn(:,1)
             Input_index_coll=[];
