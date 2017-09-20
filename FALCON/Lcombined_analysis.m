7dc89d3935300cad0f6ec4f6a1970f5a9fb9df6d
philippelucarelli <philippe.lucarelli@uni.lu>
2017-09-20 15:05:44 +0200

    beta version
    Automatic analysis of the different Regularizations

---
+++ FALCON/Lcombined_analysis.m
@@ -0,0 +1,138 @@
+function Lcombined_analysis(estim, M, ListLambda1, ListLambda2, PowerLambda1, PowerLambda2, vartest ,FinalFolderName)
+
+%%%%%%%%%%%% TO DO %%%%%%%%%%%%
+% % get the ranking of the most important parameters
+% % additional selection criterion
+%%%%%%%%%%%% %%%%%%%%%%%%
+
+
+%% Analyis of combined L1 and L1/2 regularization
+t= num2cell(vartest) 
+[PlotFitSummary, PlotFitIndividual, PlotHeatmapCost, PlotStateSummary, PlotStateEvolution] = deal(t{:})
+
+
+%% Overview of the data analysis with general plots
+% AIC evolution
+figure, 
+AICs=M(:,end-3);
+AICs=reshape(AICs,length(ListLambda1),length(ListLambda2));
+imagesc(AICs);
+title('AIC')
+set(gca,'XTick',1:length(ListLambda1))
+set(gca,'xticklabels',['  0'; num2str(PowerLambda2(:))])
+set(gca,'xticklabelrotation',90)
+set(gca,'YTick',1:length(ListLambda2))
+set(gca,'yticklabels',['  0'; num2str(PowerLambda1(:))])
+ylabel('L1 grouped')
+xlabel('L1/2')
+
+% BIC evolution
+figure, 
+BICs=M(:,end-2);
+BICs=reshape(BICs,length(ListLambda1),length(ListLambda2));
+imagesc(BICs);
+title('BIC')
+set(gca,'XTick',1:length(ListLambda1))
+set(gca,'xticklabels',['  0'; num2str(PowerLambda2(:))])
+set(gca,'xticklabelrotation',90)
+set(gca,'YTick',1:length(ListLambda2))
+set(gca,'yticklabels',['  0'; num2str(PowerLambda1(:))])
+ylabel('L1 grouped')
+xlabel('L1/2')
+
+% MSE evolution
+
+figure,
+MSEs=M(:,end-2);
+MSEs=reshape(MSEs,length(ListLambda1),length(ListLambda2));
+imagesc(log(MSEs))
+title('MSE L1 vs L1/2 regularization')
+ylabel('L1 grouped')
+xlabel('L1/2')
+
+% parameter evolution
+figure, 
+Nps=M(:,end);
+Nps=reshape(Nps,length(ListLambda1),length(ListLambda2));
+imagesc(Nps)
+title('#Parameters')
+ylabel('L1 grouped')
+xlabel('L1/2')
+
+%%% SBC
+SBC= numel(estim.Output) .* log(M(:,1)) + M(:,end) .* log(numel(estim.Output));
+figure, 
+SBCs=reshape(SBC,length(ListLambda1),length(ListLambda2));
+imagesc(SBCs)
+title('SBC')
+set(gca,'XTick',1:length(ListLambda1))
+set(gca,'xticklabels',['  0'; num2str(PowerLambda2(:))])
+set(gca,'xticklabelrotation',90)
+set(gca,'YTick',1:length(ListLambda2))
+set(gca,'yticklabels',['  0'; num2str(PowerLambda1(:))])
+ylabel('L1 grouped')
+xlabel('L1/2')
+
+
+%% specific data analysis
+
+% find minimal AIC
+min_AIC = min(M(:,end-3));
+AIC_pos= find(ismember(M(:,end-3),min_AIC));
+
+% plot model fits for best AIC
+[MeanStateValueAll, StdStateValueAll, MeanCostAll, StdCostAll, estim] = FalconSimul(estim,M(AIC_pos,2:end-5),[PlotFitSummary, PlotFitIndividual, PlotHeatmapCost, PlotStateSummary, PlotStateEvolution],FinalFolderName);
+
+
+ 
+best_dataLambda = M(AIC_pos,2:end-5);
+
+k=1;
+counter=1;
+parameterValues_mean=[];
+parameterValues_std=[];
+for k=1:length(best_dataLambda)/length(ContextsList)
+    data=best_dataLambda(1,counter:counter+length(ContextsList)-1);
+    test_mean_std(k,1)=mean(data);
+    test_mean_std(k,2)=std(data);
+    counter=counter+length(ContextsList);
+    k=k+1;
+end
+test_mean_std=sortrows(test_mean_std);
+figure; errorbar(test_mean_std(:,1),test_mean_std(:,2))
+xlabel('parameters')
+ylabel('parameter means')
+title(['Parameter means and standard error at AIC=' num2str(min_AIC)])  
+
+
+%% identification of crucial parameters based on L1_groups
+
+% find minimal AIC
+min_AIC = min(M(:,end-3));
+AIC_pos= find(ismember(M(:,end-3),min_AIC));
+ 
+
+Full_M = M(:,2:end-5);
+%dataLambda4u16=M(410,2:196)
+
+parameterValues_mean=[];
+parameterValues_std=[];
+for rows= 1: size(M,1)
+    k=1;
+    counter=1;
+    for k=1:length(Full_M(rows,:))/length(ContextsList)
+        data=Full_M(rows,counter:counter+length(ContextsList)-1);
+        test_mean(rows,k)=mean(data);
+        test_std(rows,k)=std(data);
+        counter=counter+length(ContextsList);
+        k=k+1;
+    end
+end
+% test_std_sort=sortrows(test_std(:,1))
+
+figure; plot(test_std(1:length(ListLambda2),:));
+ylabel('standard deviation')
+xlabel('Lambda')
+title(['Regularization dynamics'])  
+
+end
