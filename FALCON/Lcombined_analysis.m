b4d2e530642b8999c34c15a3ad12e8bad49159b0
Seb <sebdelandtsheer@gmail.com>
2017-10-02 17:25:04 +0200

    Update L1 group analysis

--- FALCON/Lcombined_analysis.m
+++ FALCON/Lcombined_analysis.m
@@ -5,7 +5,9 @@ function Lcombined_analysis(estim,ContextsList, M, ListLambda12, ListLambda1grou
 % % additional selection criterion
 %%%%%%%%%%%% %%%%%%%%%%%%
 
-
+        clear all, clc,
+        load('Lcombined.mat')
+        
 %% Analyis of combined L1 and L1/2 regularization
 t= num2cell(vartest) 
 [PlotFitSummary, PlotFitIndividual, PlotHeatmapCost, PlotStateSummary, PlotStateEvolution] = deal(t{:})
@@ -19,10 +21,10 @@ AICs=reshape(AICs,length(ListLambda12),length(ListLambda1group));
 imagesc(AICs);
 title('AIC')
 set(gca,'XTick',1:length(ListLambda12))
-set(gca,'xticklabels',['0'; num2str(PowerLambda1group(:))])
+set(gca,'xticklabels',num2str([0; PowerLambda1group(:)]))
 set(gca,'xticklabelrotation',90)
 set(gca,'YTick',1:length(ListLambda1group))
-set(gca,'yticklabels',['0'; num2str(PowerLambda12(:))])
+set(gca,'yticklabels',num2str([0; PowerLambda12(:)]))
 ylabel('L1 grouped')
 xlabel('L1/2')
 
@@ -33,10 +35,10 @@ BICs=reshape(BICs,length(ListLambda12),length(ListLambda1group));
 imagesc(BICs);
 title('BIC')
 set(gca,'XTick',1:length(ListLambda12))
-set(gca,'xticklabels',['0'; num2str(PowerLambda1group(:))])
+set(gca,'xticklabels',num2str([0, PowerLambda1group(:)]))
 set(gca,'xticklabelrotation',90)
 set(gca,'YTick',1:length(ListLambda1group))
-set(gca,'yticklabels',['0'; num2str(PowerLambda12(:))])
+set(gca,'yticklabels',num2str([0, PowerLambda12(:)]))
 ylabel('L1 grouped')
 xlabel('L1/2')
 
@@ -47,6 +49,11 @@ MSEs=M(:,end-2);
 MSEs=reshape(MSEs,length(ListLambda12),length(ListLambda1group));
 imagesc(log(MSEs))
 title('MSE L1 vs L1/2 regularization')
+set(gca,'XTick',1:length(ListLambda12))
+set(gca,'xticklabels',['  0'; num2str(PowerLambda1group(:))])
+set(gca,'xticklabelrotation',90)
+set(gca,'YTick',1:length(ListLambda1group))
+set(gca,'yticklabels',['  0'; num2str(PowerLambda12(:))])
 ylabel('L1 grouped')
 xlabel('L1/2')
 
@@ -56,6 +63,11 @@ Nps=M(:,end);
 Nps=reshape(Nps,length(ListLambda12),length(ListLambda1group));
 imagesc(Nps)
 title('#Parameters')
+set(gca,'XTick',1:length(ListLambda12))
+set(gca,'xticklabels',['  0'; num2str(PowerLambda1group(:))])
+set(gca,'xticklabelrotation',90)
+set(gca,'YTick',1:length(ListLambda1group))
+set(gca,'yticklabels',['  0'; num2str(PowerLambda12(:))])
 ylabel('L1 grouped')
 xlabel('L1/2')
 
@@ -66,10 +78,10 @@ SBCs=reshape(SBC,length(ListLambda12),length(ListLambda1group));
 imagesc(SBCs)
 title('SBC')
 set(gca,'XTick',1:length(ListLambda12))
-set(gca,'xticklabels',['0'; num2str(PowerLambda1group(:))])
+set(gca,'xticklabels',num2str([0, PowerLambda1group(:)]))
 set(gca,'xticklabelrotation',90)
 set(gca,'YTick',1:length(ListLambda1group))
-set(gca,'yticklabels',['0'; num2str(PowerLambda12(:))])
+set(gca,'yticklabels',num2str([0, PowerLambda12(:)]))
 ylabel('L1 grouped')
 xlabel('L1/2')
 
@@ -78,62 +90,65 @@ xlabel('L1/2')
 
 % find minimal AIC
 min_AIC = min(M(:,end-3));
-AIC_pos= find(ismember(M(:,end-3),min_AIC));
+AIC_pos = find(ismember(M(:,end-3),min_AIC));
 
 % plot model fits for best AIC
 [MeanStateValueAll, StdStateValueAll, MeanCostAll, StdCostAll, estim] = FalconSimul(estim,M(AIC_pos,2:end-5),[PlotFitSummary, PlotFitIndividual, PlotHeatmapCost, PlotStateSummary, PlotStateEvolution],FinalFolderName);
 
+best = M(AIC_pos,2:end-5);
 
- 
-best_dataLambda = M(AIC_pos,2:end-5);
+best = best(estim.RegMatrix);
+best_mean=mean(best, 2);
+best_std=std(best, 0, 2);
 
-k=1;
-counter=1;
-parameterValues_mean=[];
-parameterValues_std=[];
-for k=1:length(best_dataLambda)/length(ContextsList)
-    data=best_dataLambda(1,counter:counter+length(ContextsList)-1);
-    test_mean_std(k,1)=mean(data);
-    test_mean_std(k,2)=std(data);
-    counter=counter+length(ContextsList);
-    k=k+1;
-end
-test_mean_std=sortrows(test_mean_std);
-figure; errorbar(test_mean_std(:,1),test_mean_std(:,2))
+
+
+[best_std, Order] = sortrows(best_std);
+figure, 
+bar(best_mean), hold on
+errorbar(best_mean,best_std, '.k')
+set(gca, 'xtick', 1:length(best_std))
+set(gca, 'xticklabels', estim.param_vector(Order))
+set(gca, 'xticklabelrotation', 90)
 xlabel('parameters')
 ylabel('parameter means')
-title(['Parameter means and standard error at AIC=' num2str(min_AIC)])  
+title(['Parameter means and standard error at AIC=' num2str(min_AIC)])
 
 
 %% identification of crucial parameters based on L1_groups
+    
+ref=FalconMakeModel(InputFile, MeasFile, 0.5)
+Mgroup=M(1:length(ListLambda1group),2:end-5);
 
-% find minimal AIC
-min_AIC = min(M(:,end-3));
-AIC_pos= find(ismember(M(:,end-3),min_AIC));
- 
-
-Full_M = M(:,2:end-5);
-%dataLambda4u16=M(410,2:196)
-
-parameterValues_mean=[];
-parameterValues_std=[];
-for rows= 1: size(M,1)
-    k=1;
-    counter=1;
-    for k=1:length(Full_M(rows,:))/length(ContextsList)
-        data=Full_M(rows,counter:counter+length(ContextsList)-1);
-        test_mean(rows,k)=mean(data);
-        test_std(rows,k)=std(data);
-        counter=counter+length(ContextsList);
-        k=k+1;
+L=length(ContextsList);
+N=size(Mgroup,2)/L;
+STDs=zeros(size(Mgroup,1),N);
+for g=1:size(Mgroup,1)
+    for c=1:N
+        Idx= 1+((c-1)*L):1+((c-1)*L)+L-1;
+        STDs(g, c)=std(Mgroup(g, Idx));
     end
 end
-% test_std_sort=sortrows(test_std(:,1))
 
-figure; plot(test_std(1:length(ListLambda1group),:));
-ylabel('standard deviation')
-xlabel('Lambda')
-title(['Regularization dynamics'])  
+figure, imagesc(STDs)
+
+Threshold=0.01;
+STDs_pass=STDs<Threshold;
+STDs_x=STDs_pass(1:end-1,:)~=STDs_pass(2:end,:)
+[row col] = find(STDs_x)
+
+ResultsLambda=[];
+ResultsParams={};
+for c=size(Mgroup,1):-1:1
+    Idx=find(row==c);
+    if ~isempty(Idx)
+        ResultsLambda=[ResultsLambda; PowerLambda1group(c)];
+        ResultsParams=[ResultsParams; {ref.param_vector(Idx)'}]
+    end
+end
+
+
+%%
 
 % get for each param the value where STD  is below 1%
 
