b7dbc06c0f6783c458873f8126c69ea6b8336b0b
Seb <sebdelandtsheer@gmail.com>
2018-01-24 16:38:51 +0100

    small function to compute uniformity

---
+++ FALCON/unif.m
@@ -0,0 +1,28 @@
+function [U]=unif(X)
+%%% [U]=unif(X) computes the uniformity of the M by P matrix and returns
+%%% the 1 by P vector of uniformities for each vector of M values.
+%%% Uniformity is defined as the inverse of the average absolute deviation
+%%% from the local density of values. Vectors of exactly equidistant values
+%%% have infinite uniformity.
+%%% This function considers all column vectors to be independent. To
+%%% compute the uniformity of a P-dimensional matrix, the (weighted)
+%%% average of the uniformities needs to be computed.
+%%% Reference: De Landtsheer et al., Frontiers in Physiology, ...
+%%% sebastien.delandtsheer@uni.lu
+%%% sebdelandtsheer@gmail.com
+%%% thomas.sauter@uni.lu
+
+
+[T,S]=size(X);
+X=sort(X,1,'ascend');
+Xprime=X';
+U=zeros(S,1);
+for c=1:T-1
+   for cc=c+1:T
+        d=(Xprime(:,cc)-Xprime(:,c))-(cc-c)/T;
+        U=U+abs(d);
+   end
+end
+U=T./U;
+
+end
